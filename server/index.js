73332affdf0693e1418023510d90aef6ba4530fe
Shlok Shah <shlokshah412@gmail.com>
2026-08-16 00:04:59 +0530

    Add cache-busting v2.6 versioning and no-cache headers for instant mobile updates

--- server/index.js
+++ server/index.js
@@ -32,7 +32,19 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public')));
+
+// Force Safari and mobile browsers to never serve stale cached JS/CSS
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
+
+app.use(express.static(path.join(__dirname, '../public'), {
+  etag: false,
+  maxAge: 0
+}));
 
 // Helper to get local network IP address
 function getLocalIpAddress() {
